ca6fc5a2cce50a1394727a1b6bc65b445461acba
Claudio Carvalho <carvalhooclaudio@gmail.com>
2020-10-13 18:13:07 -0300

    Updates

---
+++ precache-manifest.0c42d8804061d8f5fb7183cc0cf8e8af.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "606a5f65a2186c5f7ea1cde6c59d2bbc",
+    "url": "/luiza-piecharts/index.html"
+  },
+  {
+    "revision": "f80ea500f7e7b70b4ceb",
+    "url": "/luiza-piecharts/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "2fd19abfc37c7940b98e",
+    "url": "/luiza-piecharts/static/js/2.d3e8bb35.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/luiza-piecharts/static/js/2.d3e8bb35.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f80ea500f7e7b70b4ceb",
+    "url": "/luiza-piecharts/static/js/main.03b59fca.chunk.js"
+  },
+  {
+    "revision": "c8fbf65bd4c504e0911e",
+    "url": "/luiza-piecharts/static/js/runtime-main.4d7e341a.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/luiza-piecharts/static/media/logo.5d5d9eef.svg"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/luiza-piecharts/precache-manifest.0c42d8804061d8f5fb7183cc0cf8e8af.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/luiza-piecharts/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
+});
